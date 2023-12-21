6273bfcc189f7112d30fac389e1a432cb47e9406
jpbacher <bacherjp@gmail.com>
2023-12-21 07:47:43 -0500

    added add'l arg to function

--- 1.0-Client/local.py
+++ 1.0-Client/local.py
@@ -6,12 +6,20 @@ import requests
 PROJ_ROOT = os.path.join(os.pardir)
 data_path = os.path.join(PROJ_ROOT, 'data', 'ecommerce.csv')
 
-def read_csv_to_json(filepath, header_row = None):
+def read_csv_to_json(filepath, columns, header_row = None):
 
     """Converts csv file to json doc
 
     Args:
         filepath (string): file path to the csv
+        columns (list): column names
         header_row (Boolean): whether file has header row 
     """
+
+    with open(data_path, 'r') as csvfile:
+        reader = csv.DictReader(csvfile, fieldnames=columns)
+        if header_row:
+            next(reader)
+        for i, row in enumerate(reader):
+            
 
